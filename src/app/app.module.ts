5fa849060f52abbf706141d16e7eba57dbff073f
Oleksii Chumak <chumak.ac@gmail.com>
2019-09-28 15:07:09 +0300

    Properties editor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {AceEditorModule} from 'ng2-ace-editor';
 
 import { AppComponent } from './app.component';
+import { PropertiesEditorComponent } from './properties-editor/properties-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertiesEditorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AceEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/properties-editor/properties-editor.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import 'brace/mode/properties';
+
+@Component({
+  selector: 'app-properties-editor',
+  templateUrl: './properties-editor.component.html',
+  styleUrls: ['./properties-editor.component.css']
+})
+export class PropertiesEditorComponent implements OnInit {
+
+  options: any = {
+    showLineNumbers: true,
+    tabSize: 2
+  };
+  text = `#Thu Apr 11 17:37:58 SRET 2019
+db.user=mkyong
+db.password=password
+db.url=localhost`;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+}
