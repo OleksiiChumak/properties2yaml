161189ee91b2236db05c49288fd09b96e07590b8
Oleksii Chumak <chumak.ac@gmail.com>
2019-10-19 20:01:54 +0300

    Parse properties

--- src/app/utils/properties-utils.spec.ts
+++ src/app/utils/properties-utils.spec.ts
@@ -1,7 +1,79 @@
-import {stringifyProperties} from './properties-utils';
+import {parseProperties, stringifyProperties} from './properties-utils';
 import {safeLoad} from 'js-yaml';
 
 describe('PropertiesUtils', () => {
+
+  it('should parse path property', () => {
+    const testObj = 'path=c:\\\\wiki\\\\templates';
+    const res = parseProperties(testObj);
+    expect(res).toEqual({
+      path: 'c:\\wiki\\templates'
+    });
+  });
+
+  it('should parse unicode property', () => {
+    const testObj = 'tab : \\u0009';
+    const res = parseProperties(testObj);
+    expect(res).toEqual({
+      tab: '\t'
+    });
+  });
+
+  it('should not parse unicode property', () => {
+    const testObj = 'tab : \\u000';
+    const res = parseProperties(testObj);
+    expect(res).toEqual({
+      tab: '\\u000'
+    });
+  });
+
+  it('should parse properties', () => {
+    const testObj = `# You are reading the ".properties" entry.
+! The exclamation mark can also mark text as comments.
+# The key characters =, and : should be written with
+# a preceding backslash to ensure that they are properly loaded.
+# However, there is no need to precede the value characters =, and : by a backslash.
+website = https://en.wikipedia.org/
+language = English
+# The backslash below tells the application to continue reading
+# the value onto the next line.
+message = Welcome to \\
+          Wikipedia!
+# Add spaces to the key
+key\\ with\\ spaces = This is the value that could be looked up with the key "key with spaces".
+aa\\taa:xx
+# Unicode
+tab : \\u0009
+# If you want your property to include a backslash, it should be escaped by another backslash
+path=c:\\\\wiki\\\\templates
+# However, some editors will handle this automatically
+   multiline:line1\\n\\
+line2
+novalue:
+:nokey
+duplicate:val1
+duplicate:val2
+bbbb
+#==============
+lll\\
+2222:fefefef`;
+    const res = parseProperties(testObj);
+    expect(res).toEqual({
+      website: 'https://en.wikipedia.org/',
+      tab: '\t',
+      'aa\taa': 'xx',
+      multiline: 'line1\nline2',
+      bbbb: '',
+      language: 'English',
+      novalue: '',
+      duplicate: 'val2',
+      'key with spaces': 'This is the value that could be looked up with the key "key with spaces".',
+      lll2222: 'fefefef',
+      '': 'nokey',
+      path: 'c:\\wiki\\templates',
+      message: 'Welcome to Wikipedia!'
+    });
+  });
 
   it('should convert simple property', () => {
     const testObj = 'xxxx';
--- src/app/utils/properties-utils.ts
+++ src/app/utils/properties-utils.ts
@@ -3,12 +3,97 @@ export class PropertiesError extends Error {
 }
 
 const DEFAULT_PROPERTY_NAME = 'value';
+const DEFAULT_PROPERTY_KEY_VALUE_SEPARATOR = '=';
 
 export function stringifyProperties(obj: any): string {
   if (isSimpleValue(obj)) {
     return toProperty(DEFAULT_PROPERTY_NAME, obj);
   }
   return propertyToString(obj).join('\n');
+}
+
+export function parseProperties(props: string): any {
+  const lines: string[] = [];
+  let nextLine = 0;
+  props.split('\n').forEach(line => {
+    const trimmedLine = line.trim();
+    if (trimmedLine.length === 0 || trimmedLine[0] === '#' || trimmedLine[0] === '!') {
+      return;
+    }
+    if (trimmedLine[trimmedLine.length - 1] === '\\') {
+      appendToCurrentLine(lines, nextLine, trimmedLine.substr(0, trimmedLine.length - 1));
+    } else {
+      appendToCurrentLine(lines, nextLine++, trimmedLine);
+    }
+  });
+  const res = {};
+  lines.map(value => normalizeKeyValueSeparator(value))
+    .map(value => splitToKeyValue(value))
+    .map(value => [stringToSimpleValue(value[0]), stringToSimpleValue(value[1])])
+    .forEach(value => res[value[0]] = value[1]);
+  return res;
+}
+
+function stringToSimpleValue(value: string): any {
+  let res = '';
+  for (let i = 0; i < value.length; i++) {
+    let nextAppend: string;
+    const currentChar = value[i];
+    if (currentChar === '\\' && i !== value.length - 1) {
+      const nextChar = value[++i];
+      switch (nextChar) {
+        case 'n':
+          nextAppend = '\n';
+          break;
+        case 't':
+          nextAppend = '\t';
+          break;
+        case 'u':
+          if (i < value.length - 4) {
+            const charCodeStr = value.substr(i + 1, i + 6);
+            nextAppend = String.fromCodePoint(parseInt(charCodeStr, 16));
+            i += 4;
+          } else {
+            nextAppend = '\\' + nextChar;
+          }
+          break;
+        default:
+          nextAppend = nextChar;
+          break;
+      }
+    } else {
+      nextAppend = currentChar;
+    }
+    res += nextAppend;
+  }
+  return res;
+}
+
+
+function splitToKeyValue(line: string): [string, string] {
+  const separatorIndex = line.indexOf(DEFAULT_PROPERTY_KEY_VALUE_SEPARATOR);
+  if (separatorIndex === -1) {
+    return [line.trim(), ''];
+  } else {
+    return [line.substr(0, separatorIndex).trim(), line.substr(separatorIndex + 1).trim()];
+  }
+}
+
+function normalizeKeyValueSeparator(line: string): string {
+  const colonIndex = line.indexOf(':');
+  const separatorIndex = line.indexOf(DEFAULT_PROPERTY_KEY_VALUE_SEPARATOR);
+  if (colonIndex !== -1 && (colonIndex < separatorIndex || separatorIndex === -1)) {
+    return line.replace(':', DEFAULT_PROPERTY_KEY_VALUE_SEPARATOR);
+  }
+  return line;
+}
+
+function appendToCurrentLine(lines: string[], currentLine: number, line: string): void {
+  if (typeof lines[currentLine] === 'undefined') {
+    lines[currentLine] = line;
+  } else {
+    lines[currentLine] += line;
+  }
 }
 
 function propertyToString(obj: any, prefix: string = ''): string[] {
@@ -30,7 +115,7 @@ function propertyToString(obj: any, prefix: string = ''): string[] {
 }
 
 function toProperty(key: string, value: any) {
-  return `${key}=${simpleValueToString(value)}`;
+  return `${key}${DEFAULT_PROPERTY_KEY_VALUE_SEPARATOR}${simpleValueToString(value)}`;
 }
 
 function simpleValueToString(obj: any): string {
